3e322cb2093b2724b68c58df648f164837ca0f62
ihab_tawffiq <ihab.tawffiq@outlook.com>
2019-10-13 22:03:52 +0200

    d

--- src/components/Student.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Student = () =>{
-    return (
-        <div>
-            <h1 className="text-center">Student</h1>
-        </div>
-    )
-}
-export default Student
