839e0fa5c69b70ce6714f134cfba3092c875c8c6
ihab_tawffiq <ihab.tawffiq@outlook.com>
2019-11-06 18:18:32 +0200

    remove old src and public

--- src/components/Divs.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Divs = () =>{
-    return (
-        <div>
-            <div className="bottom-div">Bottom</div>
-        </div>
-    )
-}
-export default Divs
--- src/components/Manger.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Manger = () =>{
-    return (
-        <div>
-            <h1 className="text-center">Manger</h1>
-        </div>
-    )
-}
-export default Manger
